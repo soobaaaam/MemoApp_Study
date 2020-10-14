a5b215877c61d978309e26f256b005f0798bec3a
soobeom-jeong <soobeomjeong@gmail.com>
2020-10-14 20:32:36 +0900

    p.156 (Unexpectedly found nil 오류 발생)

---
+++ MyMemory/MemoData.swift
@@ -0,0 +1,17 @@
+//
+//  MemoData.swift
+//  MyMemory
+//
+//  Created by 정수범 on 2020/10/14.
+//  Copyright © 2020 정수범. All rights reserved.
+//
+
+import UIKit
+
+class MemoData {
+    var memoIdx : Int?
+    var title : String?
+    var contents : String?
+    var image : UIImage?
+    var regdate : Date?
+}
